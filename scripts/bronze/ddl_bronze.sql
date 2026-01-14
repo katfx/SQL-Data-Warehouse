27415f28fe61348d5feee876e41bb8d34079c30d
Katleho Ramaebeebe <katfx1356@gmail.com>
2026-01-14 12:13:55 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -58,7 +58,7 @@ GO
 
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL 
 	DROP TABLE bronze.erp_loc_a101;
-CREATE TABLE  (
+CREATE TABLE  bronze.erp_loc_a101(
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50)
 );
